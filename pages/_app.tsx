2dbb6d6dad4c31e1f2588e64eb1de2b21448bfde
HoseaLe <hoseale@163.com>
2021-04-07 22:00:40 +0800

    加载提示

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,27 @@
 import "../style.less";
 import React, { JSXElementConstructor } from "react";
+import Router from "next/router";
+import NProgress from "nprogress";
+import Head from "next/head";
+
+Router.events.on("routeChangeStart", (url) => {
+    console.log(`Loading: ${url}`);
+    NProgress.start();
+});
+Router.events.on("routeChangeComplete", () => NProgress.done());
+Router.events.on("routeChangeError", () => NProgress.done());
+
 interface Props {
     Component: JSXElementConstructor<any>;
     pageProps: { [key: string]: any };
 }
 export default function MyApp({ Component, pageProps }: Props) {
-    return <Component {...pageProps} />;
+    return (
+        <>
+            <Head>
+                <link rel="stylesheet" type="text/css" href="/nprogress.css" />
+            </Head>
+            <Component {...pageProps} />
+        </>
+    );
 }
